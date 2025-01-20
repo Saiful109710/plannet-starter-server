bdb96dc8217080c260f1b0089e25186c2a6e27bd
saiful-islam <saifulislam109710@gmail.com>
2025-01-21 01:27:56 +0600

    all done

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const jwt = require('jsonwebtoken')
 const morgan = require('morgan')
+const nodemailer = require("nodemailer");
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 
 const port = process.env.PORT || 9000
 const app = express()
@@ -36,6 +38,48 @@ const verifyToken = async (req, res, next) => {
   })
 }
 
+// send email using nodemailer
+const sendEmail = (emailAddress,emailData)=>{
+  // create transporter
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for port 465, false for other ports
+    auth: {
+      user: process.env.NODEMAILER_USER,
+      pass: process.env.NODEMAILER_PASS,
+    },
+  });
+
+  transporter.verify((error,success)=>{
+    if(error){
+      console.log(error)
+    }else{
+      console.log("Transporter is ready to email",success)
+    }
+  })
+
+  const mailBody = {
+    from:  process.env.NODEMAILER_USER, // sender address
+    to: emailAddress, // list of receivers
+    subject: emailData?.subject,
+    text: emailData?.message, // plain text body
+    html: `<p>${emailData?.message}</p>`, // html body
+  }
+
+  // send email
+
+  transporter.sendMail(mailBody,(error,info)=>{
+    if(error){
+      console.log(error)
+    }else{
+      console.log(info)
+      console.log('Email Sent:',info?.response)
+    }
+  })
+
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.92ej0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -56,6 +100,31 @@ async function run() {
     const userCollection = client.db('plant-net').collection('users')
     const plantsCollection = client.db('plant-net').collection('plants')
     const orderCollection = client.db('plant-net').collection('orders')
+
+
+    // verifyAdmin middleware
+    const verifyAdmin = async(req,res,next)=>{
+            console.log("data from verify token middleware",req.user)
+            const email = req.user?.email
+            const query={email}
+            const result = await userCollection.findOne(query)
+            if(!result || result?.role !=='admin'){
+              return res.status(403).send({message:'Forbidden Access! Admin Only'})
+            }
+            next()
+
+    }
+
+    // verify seller middleware
+    const verifySeller = async(req,res,next)=>{
+      const email = req.user?.email
+      const query = {email}
+      const result = await userCollection.findOne(query)
+      if(!result || result?.role !=="seller"){
+        return res.status(403).send({message:'Forbidden Access! Seller Only'})
+      }
+      next()
+    }
     // Generate jwt token
     app.post('/jwt', async (req, res) => {
       const email = req.body
@@ -89,6 +158,7 @@ async function run() {
      // save or update a user in db
 
   app.post('/users/:email',async(req,res)=>{
+    sendEmail()
     const email = req.params.email
     const query = {email}
     const user = req.body
@@ -135,10 +205,24 @@ async function run() {
   })
 
   // get all user
-  app.get('/all-users/:email',verifyToken,async(req,res)=>{
+  app.get('/all-users/:email',verifyToken,verifyAdmin,async(req,res)=>{
     const email = req.params.email
     const query={email:{$ne:email}}
     const result = await userCollection.find(query).toArray()
+    res.send(result)
+  })
+
+  // update user role and status
+  app.patch('/user/role/:email',verifyToken,async(req,res)=>{
+    const email = req.params.email
+    const {role} = req.body
+    const filter = {email}
+    const updatedDoc = {
+      $set:{
+        role,status:'Verified'
+      }
+    }
+    const result = await userCollection.updateOne(filter,updatedDoc)
     res.send(result)
   })
 
@@ -152,7 +236,7 @@ async function run() {
   // save plant data in db
 
 
-  app.post('/plants',verifyToken,async(req,res)=>{
+  app.post('/plants',verifyToken,verifySeller,async(req,res)=>{
     const plant = req.body
     const result = await plantsCollection.insertOne(plant)
     res.send(result)
@@ -172,6 +256,20 @@ async function run() {
     const orderInfo = req.body
     console.log(orderInfo)
     const result = await orderCollection.insertOne(orderInfo)
+    if(result?.insertedId){
+      // To Customer
+      sendEmail(orderInfo?.customerInfo?.email,{
+        subject:'Order Successful',
+        message:`You've placed an order successfully. Transaction Id: ${result?.insertedId}`
+      })
+
+      // to seller
+      sendEmail(orderInfo?.seller,{
+        subject:'You have an order to process',
+        message:`Get the plants ready for ${orderInfo?.customerInfo?.name}`
+      })
+
+    }
     res.send(result)
   })
 
@@ -239,6 +337,55 @@ async function run() {
     res.send(result)
   })
 
+  // update order status
+  app.patch('/orders/:id',verifyToken,verifySeller,async(req,res)=>{
+    const id = req.params.id
+    const {status} = req.body
+    const filter = {_id:new ObjectId(id)}
+    const updatedDoc = {
+      $set:{status}
+    }
+    const result = await orderCollection.updateOne(filter,updatedDoc)
+    res.send(result)
+  })
+
+  // get all order for specific seller using aggregate method
+  app.get('/seller-orders/:email',verifyToken,verifySeller,async(req,res)=>{
+    const email=req.params.email
+    const result = await orderCollection.aggregate([
+      {
+        $match:{seller:email}
+      },
+      {
+        $addFields:{
+          plantId:{$toObjectId:'$plantId'}
+        }
+      },
+      {
+        $lookup:{
+          from:'plants',
+          localField:'plantId',
+          foreignField:'_id',
+          as:'plants'
+        }
+      },
+      {
+        $unwind:'$plants'
+      },
+      {
+        $addFields:{
+          name:'$plants.name'
+        }
+      },
+      {
+        $project:{
+          plants:0
+        }
+      }
+    ]).toArray()
+    res.send(result)
+  })
+
 
   // cancel/delete an order api
 
@@ -251,6 +398,120 @@ async function run() {
       }
       const result = await orderCollection.deleteOne(query)
       res.send(result)
+  })
+
+  // get inventory data for seller
+  app.get('/plant/seller',verifyToken,verifySeller,async(req,res)=>{
+    const email = req.user.email
+    console.log(email)
+    const query = {'sellerInfo.email':email}
+    const result = await plantsCollection.find(query).toArray()
+    res.send(result)
+
+
+  })
+
+  // delete a plant from db by seller
+  app.delete('/plant/:id',verifyToken,verifySeller,async(req,res)=>{
+    const id = req.params.id
+    const query = {_id:new ObjectId(id)}
+    const result = await plantsCollection.deleteOne(query)
+    res.send(result)
+  })
+
+  // admin stats
+  app.get('/admin-stats',verifyToken,verifyAdmin,async(req,res)=>{
+    // get total user and total plants
+    const totalUser  = await userCollection.countDocuments()
+    const totalPlants = await plantsCollection.estimatedDocumentCount()
+
+    const allOrder = await orderCollection.find().toArray()
+    // const totalPrice = allOrder.reduce((sum,order)=>sum+order.price,0)
+    // const  totalOrder = allOrder.length
+
+    // generate chart data
+    // const myData = {
+    //   date:'11/02/2025',
+    //   quantity:4000,
+    //   price:2400,
+    //   order:2400,
+  
+    // }
+
+    const chartData = await orderCollection.aggregate([
+      {
+        $addFields:{
+          _id:{
+            $dateToString:{
+              format:'%Y-%m-%d',
+              date:{$toDate:'$_id'}
+            },
+          },
+          quantity:{
+            $sum:'$quantity'
+          },
+          price:{$sum:'$price'},
+          order:{$sum:1},
+
+          
+        }
+      },
+      {
+        $project:{
+          _id:0,
+          date:'$_id',
+          quantity:1,
+          order:1,
+          price:1
+
+        }
+      },
+      {
+        $sort:{date:-1}
+      }
+    ]).toArray()
+
+    console.log(chartData)
+
+    // get total revenue,and total order
+    const orderDetails = await orderCollection.aggregate([
+      {
+        $group:{
+          _id:null,
+          totalRevenue:{$sum:'$price'},
+          totalOrder:{$sum:1}
+        }
+      },
+      {
+        $project:{
+          _id:0
+        }
+      }
+    ]).next()
+    
+    res.send({totalPlants,totalUser,...orderDetails,chartData})
+  })
+
+  // create payment intent
+  app.post('/create-payment-intent',verifyToken,async(req,res)=>{
+    const {quantity,plantId} = req.body
+    const query={_id:new ObjectId(plantId)}
+    const plant = await plantsCollection.findOne(query)
+    if(!plant){
+      return res.status(400).send('Plant not found')
+    }
+
+    const totalPrice = (quantity * plant.price)*100 // total price in sent poysha
+    const {client_secret} = await stripe.paymentIntents.create({
+      amount: totalPrice,
+      currency: 'usd',
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
+    res.send({clientSecret:client_secret})
+
+
   })
 
     // Send a ping to confirm a successful connection
